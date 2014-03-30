5d9da7b29d578104cedc000db4a9d9e4350ee5f4
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-03-30 13:54:07 +0800

    清理

--- query.php
+++ query.php
@@ -1,18 +1,18 @@
-
-
 <html>
 <head>
 <title>connect.php</title>
 </head>
 <body>
 <?php
-/*PHP手册中的PHP连接Mysql的实例*/
-/* 连接选择数据库 */   
+phpinfo();
+//PHP手册中的PHP连接Mysql的实例
+// 连接选择数据库  
+/*print "<h1>mysql_connect</h1>\n"; 
 
 $link = mysql_connect("localhost", "root", "123456") or die("Could not connect");  
 print "Connected successfully";    
 mysql_select_db("wtb") or die("Could not select database");
-/* 执行 SQL 查询 */    
+// 执行 SQL 查询     
 
 $query = "select wtb.item.item_name,wtb.play.play_name,wtb.c,wtb.hath,wtb.num_want,wtb.src
 	from wtb.item,wtb.play,wtb.wtb 
@@ -21,7 +21,7 @@ $query = "select wtb.item.item_name,wtb.play.play_name,wtb.c,wtb.hath,wtb.num_wa
 ";   
 $result = mysql_query($query) or die("Query failed");
 
-/* 在 HTML 中打印结果 */  
+// 在 HTML 中打印结果
 print "<h1>html result</h1>\n";
 print "<table>\n";   
 while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {        
@@ -33,7 +33,7 @@ while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 } 
 print "</table>\n";
 
-/*
+
 // json api start 
 print "<h1>json string </h1>\n";
 $result = mysql_query($query) or die("Query failed");  
@@ -45,13 +45,11 @@ print "<p><code>\n";
 echo json_encode($rows);
 print "</code></p>\n";
 // json api end
-*/
-/* 释放资源 */
-
+// 释放资源 
 mysql_free_result($result);    
 
-/* 断开连接 */   
-
+// 断开连接   
 mysql_close($link);
+*/
 ?>
 </body>
