731d9a446bcfd7f4ef2a522e23e715c6b37f95e1
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-03-30 16:21:07 +0800

    update

--- q1.php
+++ q1.php
@@ -1,37 +1,49 @@
 <?php
 //PHP手册中的PHP连接Mysql的实例
-// 连接选择数据库  
-$link = mysql_connect("localhost", "root", "123456") or die("Could not connect");  
-  
-mysql_select_db("wtb") or die("Could not select database");
-// 执行 SQL 查询     
+// 连接选择数据库
+$link = mysql_connect("localhost", "root", "123456") or die("Could not connect");
 
-$query = "select wtb.item.item_name,wtb.play.play_name,wtb.c,wtb.hath,wtb.num_want,wtb.src
-	from wtb.item,wtb.play,wtb.wtb 
-	where wtb.wtb.iditem = wtb.item.iditem and wtb.wtb.idplayer = wtb.play.idplay
-	LIMIT 5;
-";   
+mysql_select_db("wtb") or die("Could not select database");
+// 执行 SQL 查询
+
+$query = "select 
+    wtb.item.item_name,
+    wtb.play.play_name,
+    wtb.play.idplay,
+    wtb.c,
+    wtb.hath,
+    wtb.num_want,
+    wtb.src
+from
+    wtb.item,
+    wtb.play,
+    wtb.wtb
+where
+    wtb.wtb.iditem = wtb.item.iditem
+        and wtb.wtb.idplayer = wtb.play.idplay
+LIMIT 5;
+";
 
 $result = mysql_query($query) or die("Query failed");
-// json api start 
+// json api start
 $rows = array();
 
-while($r = mysql_fetch_assoc($result)) {
-    $rows[] = $r;
+while ($r = mysql_fetch_assoc($result)) {
+	$rows[] = $r;
 }
 /*
-while($r = mysqli_fetch_array($result)) {
-    $rows[] = $r;
-}
-*/
+ while($r = mysqli_fetch_array($result)) {
+ $rows[] = $r;
+ }
+ */
 // show json
 echo "{\"aaData\":";
 echo json_encode($rows);
 echo "}";
 
 // json api end
-// 释放资源 
+// 释放资源
 mysql_free_result($result);
-// 断开连接   
+// 断开连接
 mysql_close($link);
 ?>
