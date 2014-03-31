7f129e2b0838ea1da9d0b18eba8831766371ee5c
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-03-31 14:37:39 +0800

    查询玩家和物品

---
+++ qitem.php
@@ -0,0 +1,19 @@
+<?php
+
+$query = "SELECT *  FROM  item;";
+//echo "var jstext=" . "'$query'";
+$link = mysql_connect("localhost", "root", "123456") or die("Could not connect");
+mysql_select_db("wtb") or die("Could not select database");
+$result = mysql_query($query) or die("Query failed");
+//echo "var reset=" . $result;
+$rows = array();
+
+while ($r = mysql_fetch_assoc($result)) {
+	$rows[] = $r;
+}
+echo json_encode($rows);
+// 释放资源
+mysql_free_result($result);
+// 断开连接
+mysql_close($link);
+?>
