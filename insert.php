235aefaa257a13b44e835ba0405868bb48a52790
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-04-01 15:16:00 +0800

    错误显示

--- insert.php
+++ insert.php
@@ -10,11 +10,12 @@ VALUES
 $query = $_POST["query"];
 //echo "var jstext='$query'"; //输出一句JS语句,生成一个JS变量,并赋颠值为PHP变量 $query 的值
 //echo "var jstext='aa'";
-echo "var jstext=" . "'$query'";
+//echo "var jstext=" . "'$query'";
 $link = mysql_connect("127.0.0.1", "root", "123456") or die("Could not connect");
 mysql_select_db("wtb") or die("Could not select database");
 $result = mysql_query($query) or die("Query failed");
-echo "var reset=" . $result;
+//echo "var reset=" . $result;
+echo $result;
 // 释放资源
 mysql_free_result($result);
 // 断开连接
