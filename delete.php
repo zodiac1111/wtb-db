ed08a9fb42447a1bb6136bb4b2f286b2dfbf6f3b
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-04-01 11:50:24 +0800

    wtb deleting...

--- delete.php
+++ delete.php
@@ -1,12 +1,12 @@
 <?php
 $wtbid = $_POST["wtbid"];
-$query="DELETE FROM `wtb`.`wtb` WHERE `idwtb`='".wtbid."';";
+$query="DELETE FROM `wtb`.`wtb` WHERE `idwtb`='".$wtbid."';";
 //echo "var jstext='$query'";
-echo "var jstext=" . "'$query'";
+//echo "var jstext=" . "'$query'";
 $link = mysql_connect("127.0.0.1", "root", "123456") or die("Could not connect");
 mysql_select_db("wtb") or die("Could not select database");
 $result = mysql_query($query) or die("Query failed");
-echo "var reset=" . $result;
+echo "query=".$query ." ; reset=" . $result;
 // 释放资源
 mysql_free_result($result);
 // 断开连接
