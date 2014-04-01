2c72d05b3b559f4c6ef33b0d0d7eeaae08c58ce7
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-04-01 15:23:00 +0800

    del

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@ $link = mysql_connect("127.0.0.1", "root", "123456") or die("Could not connect")
 mysql_select_db("wtb") or die("Could not select database");
 $result = mysql_query($query) or die("Query failed");
 //echo "query=".$query ." ; reset=" . $result;
-echo $result
+echo $result;
 // 释放资源
 mysql_free_result($result);
 // 断开连接
