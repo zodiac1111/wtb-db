e06a4bc9ed432b5189e78d848f066a4acb638151
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-04-03 21:17:31 +0800

    后端插入

--- insert.php
+++ insert.php
@@ -1,22 +1,58 @@
 <?php
 include "conf.php";
+$link = mysql_connect($mysql_host, $mysql_user, $mysql_pwd) or die("Could not connect");
+mysql_select_db($mysql_db) or die("Could not select database");
 
-$query = "INSERT 
-INTO
-   `wtb`.`wtb` (
-      `enable`,`iditem`, `idplayer`, `num_now`,`num_want`, `c`, `hath`,`src`
-   ) 
-VALUES
-   ('1', '11399', '', '1','1', '','','http://forums.e-hentai.org/');";
-$query = $_POST["query"];
+$type 	= $_POST["type"];
+$iditem	= $_POST["iditem"];
+$idplayer = $_POST["idplayer"];
+$qty 	= $_POST["qty"];
+$c	 	= $_POST["c"];
+$hath 	= $_POST["hath"];
+$note 	= $_POST["note"];
+$src	= $_POST["src"];
+
+
+$select = "select * from wtb where `type`='".$type
+		."' and `iditem`='" .$iditem ."' and `idplayer`='".$idplayer."';";
+
+// 先查询一下有没有同一玩家,同一类型(买卖),同一物品 的记录. 有则更新.没有则插入
+$result = mysql_query($select) or die("Query failed");
+$rows = array();
+while ($r = mysql_fetch_assoc($result)) {
+	$rows[] = $r;
+}
+// 有则更新
+if ($rows.length>0){
+	$idwtb=$rows[0].idwtb;
+	$query = "UPDATE 
+	   `wtb`  SET 
+		  `type`='".type."',`iditem`='".iditem."', `idplayer`='".idplayer."', `num_want`='".qty."', `c`='".c."', `hath`='".hath."',`note`='".note."',`src`='".src."'
+	WHERE `idwtb`='".$idwtb."';";
+// 没有则插入
+}else{
+	$query = "INSERT 
+	INTO
+	   `wtb` (
+		  `type`,`iditem`, `idplayer`, `num_want`, `c`, `hath`,`note`,`src`
+	   ) 
+	VALUES
+	   ('".type."', '".iditem."', '".idplayer."', '".qty."','".c."', '".hath."','".note."','".src."');";
+}
 //echo "var jstext='$query'"; //输出一句JS语句,生成一个JS变量,并赋颠值为PHP变量 $query 的值
 //echo "var jstext='aa'";
 //echo "var jstext=" . "'$query'";
-$link = mysql_connect($mysql_host, $mysql_user, $mysql_pwd) or die("Could not connect");
-mysql_select_db($mysql_db) or die("Could not select database");
+
 $result = mysql_query($query) or die("Query failed");
 //echo "var reset=" . $result;
-echo $result;
+$json->ret="1";
+$json->l=$rows.length;
+$json->select=$select;
+$json->adata="";
+$json->sql=$query;
+
+echo json_encode($json);
+
 // 释放资源
 mysql_free_result($result);
 // 断开连接
