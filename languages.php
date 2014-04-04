583fd742fd49ee3107b236183868b83d7c26bb86
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-04-04 14:12:04 +0800

    编码

--- languages.php
+++ languages.php
@@ -9,11 +9,13 @@ if (isset($_POST["locale"])) {
   $locale = "zh_CN";
   //$locale = "en_US";
 } 
+$domain="messages"; //绑定名字域??
 putenv("LC_ALL=$locale");
 setlocale(LC_ALL, $locale);
-bindtextdomain("messages", "./locale");
+bindtextdomain($domain, "./locale");
 bind_textdomain_codeset($domain ,  'UTF-8' );  //设置mo文件的编码为UTF-8   
-textdomain("messages");
+textdomain($domain);
+header('Content-Type: text/html; charset=utf-8');
 /*
 function _($argument) {
     return echo \_($argument); // this would be the non-overloaded _()
