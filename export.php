21f92aaad906b06acf588460de4cb8f8d8e9eaaf
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-04-07 01:34:33 +0800

    导出的问题

--- export.php
+++ export.php
@@ -2,6 +2,9 @@
 include "conf.php";
 
 $db_file="/var/www/db.gz";
+// 形如:
+// /usr/bin/mysqldump -uroot -p123456 -h127.0.0.1 wtb | /bin/gzip > /var/www/db.gz
+// 注意可能有文件(夹)权限问题.
 $cmd="/usr/bin/mysqldump -u".$mysql_user." -p".$mysql_pwd." -h".$mysql_host." ".$mysql_db." | /bin/gzip > ".$db_file;
 
 $retval=0;
