27b1bbf5d38e58f0ab32daf02516f742e55da706
zodiac1111 <zodiac8765+github_admin@gmail.com>
2014-05-01 20:51:37 +0800

    === 我不会

--- insert_do.php
+++ insert_do.php
@@ -3,29 +3,31 @@ include "conf.php";
 $link = mysql_connect($mysql_host, $mysql_user, $mysql_pwd) or die("Could not connect".mysql_error());
 mysql_select_db($mysql_db) or die("Could not select database:".$link.mysql_error());
 
-$type 	= settype($_POST["type"],"float");
-$idplayer = settype($_POST["idplayer"],"integer");
-$obj	= settype($_POST["obj"],"integer"); /// 交易的对象:物品或者装备
-$iditem	= settype($_POST["iditem"],"integer");
-$idequip= settype($_POST["idequip"],"integer");
-$qty 	= settype($_POST["qty"],"integer");
-$c	 	= settype($_POST["c"],"float");
-$hath 	= settype($_POST["hath"],"float");
+$type 	= $_POST["type"];
+$idplayer = $_POST["idplayer"];
+$obj	= $_POST["obj"]; /// 交易的对象:物品或者装备
+$iditem	= $_POST["iditem"];
+$idequip= $_POST["idequip"];
+$qty 	= $_POST["qty"];
+$c	 	= $_POST["c"];
+$hath 	= $_POST["hath"];
 $note 	= $_POST["note"];
 $src	= $_POST["src"];
 $timestamp=time();
 
 /// 对象类别
-if($obj=="0"){  //物品
+if($obj==0){  //物品
 	// 先查询一下有没有同一玩家,同一类型(买卖),同一物品 的记录. 有则更新.没有则插入
 	$select = "select * from `order` where `type`='".$type
 		."' and `obj`='" .$obj 
 		."' and `iditem`='" .$iditem ."' and `idplayer`='".$idplayer."';";
-}else if($obj=="1"){ //装备
+}else if($obj==1){ //装备
 	// 先查询一下有没有同一玩家,同一类型(买卖),同一物品 的记录. 有则更新.没有则插入
 	$select = "select * from `order` where `type`='".$type
 		."' and `obj`='" .$obj 
 		."' and `idequip`='" .$idequip ."' and `idplayer`='".$idplayer."';";
+}else{
+	$select = "unknow type obj!";
 }
 
 
@@ -38,13 +40,13 @@ $rows = mysql_fetch_assoc($result);
 
 // 没有则插入
 if (empty($rows)){
-	if($obj=="0"){  //物品
+	if($obj==0){  //物品
 		$query = "INSERT INTO `order` ( "
 			." `type`,`obj`,`iditem`, `idplayer`, `qty`, `c`, `hath`,`note`,`src`,`timestamp`) " 
 			." VALUES "
 			." ('".$type."','".$obj."','".$iditem."','".$idplayer."','"
 			.$qty."','".$c."','".$hath."','".$note."','".$src."','".$timestamp."');";
-	}else if($obj=="1"){ //装备
+	}else if($obj==1){ //装备
 		$query = "INSERT INTO `order` ( "
 			." `type`,`obj`,`idequip`, `idplayer`, `qty`, `c`, `hath`,`note`,`src`,`timestamp`) " 
 			." VALUES "
@@ -55,7 +57,7 @@ if (empty($rows)){
 }else{
 	//  索引关联数组
 	$idwtb=$rows["idwtb"];
-	if($obj=="0"){  //物品
+	if($obj==0){  //物品
 		$query = "UPDATE `order` SET "
 			." `type`='".$type."',`obj`='".$obj
 			."',`iditem`='".$iditem
@@ -63,7 +65,7 @@ if (empty($rows)){
 			."',`c`='".$c."', `hath`='".$hath."',`note`='".$note
 			."',`src`='".$src."',`timestamp`='".$timestamp."' "
 			." WHERE `idwtb`='".$idwtb."';";
-	}else if($obj=="1"){ //装备
+	}else if($obj==1){ //装备
 		$query = "UPDATE `order` SET "
 			." `type`='".$type."',`obj`='".$obj
 			."',`idequip`='".$idequip
